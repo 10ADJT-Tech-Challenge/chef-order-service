8337c0d37e51fbc7d6a39a2e39ac241a224a0f5c
LucasSouza <lsouza.prog@outlook.com>
2026-03-23 19:02:34 -0300

    fix(auth-assimetrica): fix novamente, faltaram imports, erro na ide gerou esses problemas

--- src/main/java/com/adjt/cheforderapi/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/adjt/cheforderapi/infrastructure/security/SecurityConfig.java
@@ -1,8 +1,20 @@
+package com.adjt.cheforderapi.infrastructure.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+import java.security.interfaces.RSAPublicKey;
 @Configuration
 public class SecurityConfig {
 
     private final RSAPublicKey publicKey;
     private final JwtAuthenticationEntryPoint authenticationEntryPoint;
+    private final JwtAccessDeniedHandler accessDeniedHandler;
 
     public SecurityConfig(
             RSAPublicKey publicKey,
